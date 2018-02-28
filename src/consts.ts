4d1d7435167713810e65ac39bb54f11b3c2fd9be
alejo <alejandro@skyhitzmusic.com>
2018-02-28 17:29:10 -0500

    tx

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SLACK_API_TOKEN = 'xoxb-318335231863-kPmbBdXCsBRnS82fUSsZlydD';
+export const SLACK_API_TOKEN = process.env.SLACK_API_TOKEN;
 export const HORIZON_URL = process.env.NODE_ENV === 'dev' ? 'https://horizon-testnet.stellar.org' : 'https://horizon.stellar.org';
