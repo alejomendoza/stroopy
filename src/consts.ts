c41130ddab4888c5e7a88827806ed17fc26fb1b5
alejo <alejandro@skyhitzmusic.com>
2018-03-12 15:09:45 -0500

    add slack button integration for multiple teams

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,4 @@
-export const BOT_TOKEN = process.env.BOT_TOKEN;
+export const CLIENT_ID = process.env.CLIENT_ID;
+export const CLIENT_SECRET = process.env.CLIENT_SECRET;
+export const PORT = '3000';
 export const HORIZON_URL = process.env.NODE_ENV === 'dev' ? 'https://horizon-testnet.stellar.org' : 'https://horizon.stellar.org';
